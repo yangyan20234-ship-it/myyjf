842c695b82709c38ee630a64507f430345045f98
yangyan20234-ship-it <yangyan20234-ship-it@users.noreply.github.com>
2026-05-02 06:46:06 +0800

    qk

--- test_sample.py
+++
@@ -1,9 +0,0 @@
-"""Simple test file created by Codex for repository smoke testing."""
-
-def add(a: int, b: int) -> int:
-    """Return the sum of two integers."""
-    return a + b
-
-
-if __name__ == "__main__":
-    print(add(2, 3))
