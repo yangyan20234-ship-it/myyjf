34a5ead0d58341e3a877fb30036dcb12b9123363
yangyan20234-ship-it <yangyan20234-ship-it@users.noreply.github.com>
2026-05-03 10:20:55 +0800

    日常更新

--- bk/posts.js
+++ bk/posts.js
@@ -1,7 +1,4 @@
 const posts = [
-    { slug: 'post-1777772531', title: '遗迹', date: '2026-05-03' },
     { slug: 'dog', title: '狗狗故事', date: '2026-05-03' },
-    { slug: 'yjd', title: '哈哈', date: '2026-05-03' },
-    { slug: 'tex', title: 'mmk', date: '2026-05-02' },
     
 ];
