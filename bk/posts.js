37df106dd5f29686f5e7e7895c8144623841349c
yangyan20234-ship-it <yangyan20234-ship-it@users.noreply.github.com>
2026-05-03 20:05:55 +0800

    日常更新

--- bk/posts.js
+++ bk/posts.js
@@ -1,4 +1,5 @@
 const posts = [
+    { slug: 'rj', title: '日记', date: '2026-05-03' },
     { slug: 'dog', title: '狗狗故事', date: '2026-05-03' },
     
     { slug: 'yjd', title: 'you你d', date: '2026-05-03' },
