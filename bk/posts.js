c46dc394b18c63d55e395869c69a7eed82aa03e6
yangyan20234-ship-it <yangyan20234@gmail.com>
2026-05-03 10:36:54 +0800

    更新 posts.js

--- bk/posts.js
+++ bk/posts.js
@@ -1,5 +1,5 @@
 const posts = [
     { slug: 'dog', title: '狗狗故事', date: '2026-05-03' },
     
-    { slug: 'yjd ', title: 'yjd', date: '2026-05-03' },
+    { slug: 'yjd', title: 'you你d', date: '2026-05-03' },
 ];
