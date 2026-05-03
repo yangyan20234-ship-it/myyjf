1f2cd982c1f94408fdb11a25c21e9e8641b5b1bd
yangyan20234-ship-it <yangyan20234-ship-it@users.noreply.github.com>
2026-05-03 08:02:41 +0800

    gg

--- bk/posts.js
+++ bk/posts.js
@@ -1,4 +1,5 @@
 const posts = [
+    { slug: 'dog', title: '狗狗故事', date: '2026-05-03' },
     { slug: 'yjd', title: '哈哈', date: '2026-05-03' },
     { slug: 'tex', title: 'mmk', date: '2026-05-02' },
     { slug: 'test_samplepy', title: '测试示例', date: '2026-05-02' },
